110ca9c6e1ed31734560f330240ce19c25860d81
Adroaldo de Andrade <adroaldof@gmail.com>
2015-05-13 16:37:03 -0300

    Remove dash from file name

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-    var markdownGist = require('./lib/markdown-gist'),
+    var markdownGist = require('./lib/markdownGist'),
         filename = process.argv[2];
 
     markdownGist.parseGitHubMarkdownFile(filename);
